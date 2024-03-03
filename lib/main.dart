a32ca4c4eb6d2ce54521f42fc8485f56df7b2b06
Munira Priyonty <preeyontyy@Muniras-MacBook-Air.local>
2024-03-03 12:46:23 +0600

    created assessment page and informations page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:welcome_299/pages/info.dart';
+import 'package:welcome_299/pages/Assessment.dart';
 import 'package:welcome_299/pages/profile.dart';
 import 'package:welcome_299/pages/LoginPage.dart';
 
@@ -14,10 +16,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Click on your profile',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/Assessment',
       routes: {
         '/':(context) => ProfileScreen(),
-        '/login': (context) => LoginPage()
+        '/login': (context) => LoginPage(),
+        '/Assessment' : (context) => Assessment(),
+        '/info' : (context) => info(),
       }
     );
   }
---
+++ lib/pages/info.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class info extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<info> {
+
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+  TextEditingController _rubrics_LinkController = TextEditingController();
+  TextEditingController _student_LinkController = TextEditingController();
+  TextEditingController _start_DateController = TextEditingController();
+  TextEditingController _end_dateController = TextEditingController();
+  
+  void _login() {
+    String title = _titleController.text;
+    String description = _descriptionController.text;
+    String rubrics_Link = _rubrics_LinkController.text;
+    String student_Link= _student_LinkController.text;
+    String start_Date = _start_DateController.text;
+    String end_date= _end_dateController.text;
+    
+    
+    // Here you can add your authentication logic
+    // For simplicity, let's just print the username and password
+    print('Title: $title');
+    print('Description: $description');
+    print('Rubrics Link: $rubrics_Link');
+    print('Student Link: $student_Link');
+    print('Start Date: $start_Date');
+    print('End Date: $end_date');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Informations'),
+        
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                labelText: 'Description',
+              ),
+            //  obscureText: true,
+            ),
+
+             SizedBox(height: 20.0),
+            TextField(
+              controller: _rubrics_LinkController,
+              decoration: InputDecoration(
+                labelText: 'Rubcrics Link',
+              ),
+            ),
+              SizedBox(height: 20.0),
+            TextField(
+              controller: _student_LinkController,
+              decoration: InputDecoration(
+                labelText: 'Student Link',
+              ),
+            ),
+              SizedBox(height: 20.0),
+            TextField(
+              controller: _start_DateController,
+              decoration: InputDecoration(
+                labelText: 'Start Date',
+              ),
+            ),
+              SizedBox(height: 20.0),
+            TextField(
+              controller: _end_dateController,
+              decoration: InputDecoration(
+                labelText: 'End Date',
+              ),
+            ),
+        //    SizedBox(height: 20.0),
+           // ElevatedButton(
+            //  onPressed: _login,
+           //   child: Text('Login'),
+         //   ),
+          ],
+        ),
+      ),
+    );
+  }
+}
